9f3e328e71fd12693c8db0da58c564737cbeed19
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-12-04 19:10:51 -0500

    Update 'Head': Add shopping cart icon

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,15 +1,27 @@
 import propTypes from 'prop-types';
 import Dropdown from '../Dropdown/Dropdown';
+import shopCart from '../../assets/img/shopping-cart.svg';
+import shopCartCheckout from '../../assets/img/shopping-cart-checkout.svg';
 import styles from './Head.module.css';
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 
 const Head = ({categories}) => {
+	const [src, setSrc] = useState(shopCart);
+
 	return (
 		<header>
 			<div className={styles.head}>
 				<h1>Loremazon</h1>
 				<input type="search" placeholder='Search' />
-				<img src={''} alt='Shopping Cart' />
+
+				<div className={styles.checkoutContainer} >
+					<img src={src} alt='Shopping Cart'
+						onMouseEnter={() => setSrc(shopCartCheckout)}
+						onMouseLeave={() => setSrc(shopCart)}
+					/>
+					<span>Checkout</span>
+				</div>
 			</div>
 
 			<nav>
